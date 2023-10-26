0970d9956b2ac1c0eff9caafb02fb55b975b1aa8
Koragm3 <hasanserro@gmail.com>
2023-10-26 20:43:43 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
+import Home from './views/Home';
+import Contacts from './views/Contacts';
+import NotFound from './views/NotFound';
 
-import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Contacts'  element={<Contacts />} />
+        <Route path= '*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
